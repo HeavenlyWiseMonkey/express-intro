1decec31b1257b75a9511daf6ef9e120ab579c41
odin <139291798+minecrafter021345@users.noreply.github.com>
2024-11-30 12:59:58 -0800

    Add views

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const app = express();
 const authorRouter = require('./routes/authorRouter');
 const bookRouter = require('./routes/bookRouter');
 const indexRouter = require('./routes/indexRouter');
+const path = require('node:path');
+
+const assetsPath = path.join(__dirname, 'public');
+app.use(express.static(assetsPath));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const links = [
+    { href: '/', text: 'Home' },
+    { href: 'about', text: 'About' },
+];
+
+const users = ['Rose', 'Cake', 'Biff'];
+
+app.get('/', (req, res) => {
+    res.render('index', { links: links, users: users });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+})
 
 app.use('/authors', authorRouter);
 app.use('/books', bookRouter);
